79826f1abdfafa34970a4424911113f228d20356
edisonivaan1 <edison.jimenez03@epn.edu.ec>
2024-12-08 22:29:19 -0500

    Botones para redirigir a PuzzleScene con una imagen especifica

--- src/Components/PuzzleDemo/Scenes/GameScene.js
+++ src/Components/PuzzleDemo/Scenes/GameScene.js
@@ -14,10 +14,17 @@ class GameScene extends Phaser.Scene {
     this.load.image('rose', '/Assets/Dibujos/rosa.png');
     this.load.image('star', '/Assets/Dibujos/estrella.png');
     this.load.image('owl', '/Assets/Dibujos/buho.png');
+    this.load.image('buttonBuho', '/Assets/Button/btnBuho.png');
+    this.load.image('buttonCirculo', '/Assets/Button/btnCirculo.png');
+    this.load.image('buttonEstrella', '/Assets/Button/btnEstrella.png');
+    this.load.image('buttonRosa', '/Assets/Button/btnRosa.png');
+
+
+    
+
   }
 
   create() {
-    // Agrega la imagen de fondo
     const background = this.add.image(0, 0, 'background').setOrigin(0, 0);
     background.displayWidth = this.sys.game.config.width; // Ajusta al ancho del canvas
     background.displayHeight = this.sys.game.config.height; // Ajusta al alto del canvas
@@ -53,7 +60,28 @@ class GameScene extends Phaser.Scene {
         console.log(`Seleccionaste: ${drawing.name}`);
       });
     });
+
+    this.createRedirectButton(1250, 915, 'buttonBuho', 'StartScene'); 
+    this.createRedirectButton(770, 430, 'buttonCirculo', 'Scene2'); 
+    this.createRedirectButton(1250, 430, 'buttonRosa', 'Scene3'); 
+    this.createRedirectButton(770, 915, 'buttonEstrella', 'Scene4'); 
   }
+
+  
+  createRedirectButton(x, y, buttonKey, sceneKey) {
+    const button = this.add.image(x, y, buttonKey).setInteractive();
+    button.setScale(0.5); // Ajusta el tamaño del botón
+
+  
+    button.setPosition(x, y);
+
+    button.on('pointerdown', () => {
+      console.log(`Redirigiendo a: ${sceneKey}`);
+      this.scene.start(sceneKey); 
+    });
+  }
+
+
 }
 
 export default GameScene;
